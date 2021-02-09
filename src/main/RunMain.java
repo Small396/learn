f6abc4787fb1958ff6e4ca39cdf8a651e5f834af
李振光 <1378943543@qq.com>
2021-02-09 13:03:10 +0800

    创建项目

---
+++ src/main/RunMain.java
@@ -0,0 +1,9 @@
+package main;
+
+/**
+ * @author: lizhenguang
+ * @date: 2021/2/9 1:01 下午
+ */
+public class RunMain {
+
+}
---
+++ src/main/learn/Learn.java
@@ -0,0 +1,9 @@
+package main.learn;
+
+/**
+ * @author: lizhenguang
+ * @date: 2021/2/9 1:00 下午
+ */
+public class Learn {
+
+}
