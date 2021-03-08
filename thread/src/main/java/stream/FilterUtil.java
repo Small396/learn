9d0aeb188bd621f50ad0b6d8e9cdd6ad22d7ca89
李振光 <1378943543@qq.com>
2021-03-08 10:15:11 +0800

    1

--- thread/src/main/java/stream/FilterUtil.java
+++ thread/src/main/java/stream/FilterUtil.java
@@ -3,9 +3,11 @@ package stream;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @author: lizhenguang
@@ -13,7 +15,7 @@ import java.util.stream.Collectors;
  */
 
 public class FilterUtil<T> {
-
+    private static String str = "abbccccaaddaggb";
     public  List<T> sizeFilter(List<T> list, int size) {
         //元素为空，直接返回空
         if (list == null || list.isEmpty()) {
@@ -22,5 +24,17 @@ public class FilterUtil<T> {
         return list.stream().limit(size).collect(Collectors.toList());
     }
 
+    public static  String toStr() {
+        char[] chars = str.toCharArray();
+        StringBuilder stringBuilder = new StringBuilder();
+        Stream.of(chars).distinct().collect(Collectors.toList()).forEach(i -> stringBuilder.append(i));
+
+        System.out.println(stringBuilder.toString());
+        return "";
+    }
+
+    public static void main(String[] args) {
+        toStr();
+    }
 
 }
