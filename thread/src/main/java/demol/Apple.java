f02230ef446ccbb65f765edd1c3b3a0e4feb6a36
李振光 <1378943543@qq.com>
2021-03-09 09:12:04 +0800

    1

--- thread/src/main/java/demol/Apple.java
+++ thread/src/main/java/demol/Apple.java
@@ -1,5 +1,6 @@
 package demol;
 
+import com.alibaba.fastjson.JSON;
 import lombok.Builder;
 import lombok.Data;
 
@@ -15,4 +16,13 @@ public class Apple {
     private String price; //价格
     private int size; //大小
     private boolean isEnter; //是否进口
+}
+ class Run{
+     public static void main(String[] args) {
+         Apple apple =  Apple.builder().color("red").price("1.22").isEnter(true).size(8).build();
+         String str = JSON.toJSONString(apple);
+         AppleDouble appleDouble =    JSON.parseObject(str,AppleDouble.class);
+         System.out.println(JSON.toJSONString(appleDouble));
+        // System.out.println(str);
+     }
 }
---
+++ thread/src/main/java/demol/AppleDouble.java
@@ -0,0 +1,16 @@
+package demol;
+
+import lombok.Data;
+
+/**
+ * @author: lizhenguang
+ * @date: 2021/3/8 6:49 下午
+ */
+@Data
+public class AppleDouble {
+
+    private String color; //颜色
+    private String price; //价格
+    private int size; //大小
+  //  private boolean isEnter; //是否进口
+}
