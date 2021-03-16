43de345fb7431df2650fb7011d67486be1d2dce4
李振光 <1378943543@qq.com>
2021-03-16 19:25:08 +0800

    类异常

--- thread/src/main/java/date/dateUtil.java
+++ thread/src/main/java/date/dateUtil.java
@@ -2,6 +2,8 @@ package date;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author: lizhenguang
@@ -11,10 +13,17 @@ public class dateUtil {
 
     public static void main(String[] args) {
         try{
+            Map<String, String> map = new HashMap<String, String>(4) {{
+                put("sing", "sing");
+                put("sing", "sing");
+                put("sing", "sing");
+            }};
+      
+            
             //保单复效时间计算
             Date toDay = new Date();
             SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            Date policyEndDay = sdf.parse("2021-03-03" + " 00:00:00");
+            Date policyEndDay = sdf.parse("2020-09-14" + " 00:00:00");
             int days = (int) ((toDay.getTime() - policyEndDay.getTime()) / (60 * 60 * 24 * 1000));
             days = (toDay.getTime() - policyEndDay.getTime()) % (60 * 60 * 24 * 1000) > 0 ? days + 1 : days;
             System.out.println(days);
