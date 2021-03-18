6be18b2b97d67f9f8be0431106d447336905ebc2
李振光 <1378943543@qq.com>
2021-03-18 14:28:28 +0800

    类异常

--- thread/src/main/java/thread/ThreadTest.java
+++ thread/src/main/java/thread/ThreadTest.java
@@ -2,6 +2,7 @@ package thread;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author: lizhenguang
@@ -11,7 +12,25 @@ public class ThreadTest {
 
  private   ThreadPoolExecutor threadPoolExecutor;
     public static void main(String[] args) throws InterruptedException {
-        long stratTime = System.currentTimeMillis();
+        CountDownLatch count = new CountDownLatch(5);
+        long start = System.currentTimeMillis();
+        for (int i = 1; i <= 5; i++) {
+            int finalI = i;
+            new Thread(() -> {
+                try {
+                    System.out.println(finalI);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    count.countDown();
+                }
+            }).start();
+        }
+        count.await();
+        System.out.println("end");
+        long end  = System.currentTimeMillis();
+        System.out.println("用时:"+String.format("%sms", end - start));
+       /* long stratTime = System.currentTimeMillis();
       //  final CountDownLatch countDownLatch = new CountDownLatch(1);
         System.out.println("统计1到100和：" + count(1));
         Thread thread001 = new Thread(() -> {
@@ -26,7 +45,7 @@ public class ThreadTest {
         System.out.println("用时："+  (endTime - stratTime)+"ms");
 
         System.out.println("thread结束了=========>>>");
-
+*/
     }
 
 
