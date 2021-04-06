eb9399ec176eb9a67efceb67cff759c5e2170861
李振光 <1378943543@qq.com>
2021-04-06 14:33:49 +0800

    1

--- thread/src/main/java/thread/ThreadTest.java
+++ thread/src/main/java/thread/ThreadTest.java
@@ -1,10 +1,10 @@
 package thread;
 
 import com.alibaba.fastjson.JSON;
-
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author: lizhenguang
@@ -12,42 +12,61 @@ import java.util.stream.Collectors;
  */
 public class ThreadTest {
 
+    private static final List aList = new ArrayList<>();
+    private static final List bList = new ArrayList<>();
+    private static volatile AtomicInteger atomicCount = new AtomicInteger();
+    /**
+     * list集合初始化
+     */
+    public static void init(int size) {
+        Random random = new Random();
+        int count = atomicCount.get();
+        while (count < 2*size) {
+            int item = random.nextInt(100);
+            if (count % 2 == 0) {
+                aList.add(item);
+                bList.add(item);
+            } else {
+                aList.add(item);
+                bList.add(random.nextInt(100));
+            }
+            atomicCount.incrementAndGet();
+        }
+        System.out.println("aList========>>" + JSON.toJSONString(aList));
+        System.out.println("bList========>>" + JSON.toJSONString(bList));
+    }
+
     /**
      * removeAll() 方法过滤
      */
-    public static void removeAll() {
-        List<String> aList = new ArrayList<>();
-        aList.add("1");
-        aList.add("2");
-        aList.add("4");
-        List<String> bList = new ArrayList<>();
-        bList.add("1");
-        bList.add("2");
-        bList.add("3");
+    public static List removeAll(final List aList, final List bList) {
+        List emptyList = new ArrayList();
+        if (bList == null) return emptyList;
+        if (aList == null || aList.size() == 0) {
+            return bList != null ? bList : emptyList;
+        }
         bList.removeAll(aList);
-        System.out.println(JSON.toJSONString(bList));
+        return bList;
     }
 
 
-    public static void  filter(){
-        List<String> list1 = new ArrayList<>();
-        list1.add("1");
-        list1.add("2");
-        list1.add("4");
-        List<String> list2 = new ArrayList<>();
-        list2.add("1");
-        list2.add("2");
-        list2.add("3");
-        list1.stream().forEach( a -> {
-            list2.stream().filter( b->  b.equals(a)).collect(Collectors.toList());
-
-                }
-        );
+    public static void filter() {
 
     }
 
     public static void main(String[] args) throws InterruptedException {
-        removeAll();
+        Thread aThread = new Thread(() -> {
+            init(5);
+            System.out.println(removeAll(aList, bList));
+        });
+        Thread bThread = new Thread(() -> {
+            init(5);
+            System.out.println(removeAll(aList, bList));
+        });
+        aThread.start();
+        //bThread.start();
+        Thread.currentThread().sleep(1000);
+        System.out.println("主线程结束===========>>>>>>");
     }
 
 }
